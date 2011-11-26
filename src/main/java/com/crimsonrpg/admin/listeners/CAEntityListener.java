5c16ff5b9637f174a26805778fc2ba38f8d10ad3
Dylan <holmes.dylan@hotmail.com>
2011-11-25 22:15:02 -0800

    ban hammer noise

--- src/main/java/com/crimsonrpg/admin/listeners/CAEntityListener.java
+++ src/main/java/com/crimsonrpg/admin/listeners/CAEntityListener.java
@@ -5,12 +5,14 @@
 package com.crimsonrpg.admin.listeners;
 
 import com.crimsonrpg.admin.CrimsonAdmin;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
+import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 /**
@@ -36,8 +38,10 @@ public class CAEntityListener extends EntityListener {
                 }
                 if (entity instanceof Player && player.getItemInHand().getType().equals(Material.FLINT)) {
                     SpoutPlayer target = (SpoutPlayer) event.getEntity();
+                    Location loc = new Location(target.getWorld(), target.getLocation().getBlockX(),target.getLocation().getBlockY(),target.getLocation().getBlockZ());
+                    SpoutManager.getSoundManager().playGlobalCustomSoundEffect(ca, "http://resources.crimsonrpg.com/s/audio/BanHammerAudio.ogg", true, loc);
                     target.setBanned(true); 
-                    target.kickPlayer("Teh BanHammer haz spokez!! :D");
+                    target.kickPlayer("Have fun enjoing your pointless life. BTW I won. :D");
                     event.setCancelled(true);
                 }
             }
